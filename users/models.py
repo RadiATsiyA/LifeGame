0e7f0c3a91abda3893f5ab01c04565ef1a1b28e5
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-06-27 12:30:05 +0300

    Deleted other models except for user models

--- users/models.py
+++ users/models.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    pass
 
 
 class Profile(models.Model):
@@ -9,59 +14,19 @@ class Profile(models.Model):
     full_name = models.CharField(max_length=150)
     avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
     notification_settings = models.JSONField()
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.id} | {self.full_name}"
 
 
-class Goal(models.Model):
-    """Model for users main goals"""
-    title = models.CharField(max_length=300)
-    description = models.TextField(max_length=3000, null=True, blank=True)
-    category = models.CharField(max_length=50)
-    created_date = models.DateField(auto_now_add=True)
-    deadline = models.DateField()
-    is_done = models.BooleanField(default=False)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.id} | {self.title}"
-
-
-class Task(models.Model):
-    """Tasks for user to achieve main goal"""
-    title = models.CharField(max_length=300)
-    description = models.TextField(max_length=3000)
-    created_date = models.DateField(auto_now_add=True)
-    deadline = models.DateField()
-    is_done = models.BooleanField(default=False)
-    goal_id = models.ForeignKey(Goal, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.title} | {self.is_done}"
-
-
-class Subtask(models.Model):
-    """Subtasks of task"""
-    title = models.CharField(max_length=300)
-    desctiprion = models.TextField(max_length=3000)
-    created_date = models.DateField(auto_now_add=True)
-    deadline = models.DateField()
-    is_done = models.BooleanField(default=False)
-    task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.title} | {self.is_done}"
-
-
 class UserLevel(models.Model):
     current_level = models.IntegerField(default=1)
     current_exp = models.IntegerField(default=0)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"User {self.user.id} | Level {self.current_level}"
+        return f"User {self.user_id.id} | Level {self.current_level}"
 
 
 class UserAchievements(models.Model):
@@ -69,30 +34,7 @@ class UserAchievements(models.Model):
     description = models.CharField(max_length=300)
     is_achieved = models.BooleanField(default=False)
     date_achieved = models.DateField(null=True, blank=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.id} | {self.title}"
-
-
-class Friendship(models.Model):
-    STATUS = [
-        ("Pending", "Pending"),
-        ("Accepted", "Accepted"),
-        ("Rejected", "Rejected")
-    ]
-
-    status = models.CharField(max_length=30, choices=STATUS, default="Pending")
-    user = models.ForeignKey(User, related_name='friendships', on_delete=models.CASCADE)
-    friend = models.ForeignKey(User, related_name='friends', on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('user', 'friend')
-
-    def __str__(self):
-        return f"{self.user.username} - {self.friend.username} ({self.status})"
-
-
-
-
-
