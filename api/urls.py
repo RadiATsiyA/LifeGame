f75708a7fa9a198776f1c0f75f8801333202897b
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-06-28 11:58:00 +0300

    rest api

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import UsersListAPIView
+
+app_name = "api"
+
+urlpatterns = [
+
+
+]
---
+++ api/views.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from users.models import CustomUser
+from .serializers import UsersSerializers
+
+# Create your views here.
+
+
